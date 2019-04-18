cb110a4445d2ae1e4dfc1c778340e861f6254430
deCodeIt <shaikhali.jafri@iitrpr.ac.in>
2019-04-18 21:27:58 +0530

    Added functionality to search for a given show in a given cinema

--- BookMyShow.py
+++ BookMyShow.py
@@ -15,8 +15,9 @@ def getObject():
 
 class BookMyShow( object ):
 
-    def __init__( self, regionCode='BANG' ):
+    def __init__( self, regionCode='BANG', date=None ):
         regionCode = regionCode.upper()
+        self.date = date
         self.ss = requests.session()
         self.setRegionDetails( regionCode )
 
@@ -39,13 +40,14 @@ class BookMyShow( object ):
         url = "https://in.bookmyshow.com/quickbook-search.bms?d[mrs]=&d[mrb]=&cat=&_=" + str( curTime ) + "&q=" + searchTerm.replace( " ", "+" ) + "&sz=8&st=ON&em=&lt=&lg=&r=" + self.regionData.get( "code" ) + "&sr="
         return url
 
-    def getUrl( self, movieData ):
+    def getMovieUrl( self, movieData ):
         '''
         `movieData` is similar to below:
 
         {'ST': 'NS', 'GRP': 'Event', 'IS_NEW': False, 'L_URL': '', 'DESC': ['Chris Evans', ' Robert Downey Jr.'], 'CODE': 'EG00068832', 'REGION_SLUG': '', 'EVENTSTRTAGS': [], 'IS_TREND': False, 'RATING': '', 'WTS': '9,14,847', 'TITLE': 'Avengers: Endgame', 'ID': 'ET00090482', 'TYPE': 'MT', 'TYPE_NAME': 'Movies', 'CAT': ''}
         '''
-        curDate = datetime.now().strftime("%Y%m%d")
+
+        curDate = datetime.now().strftime("%Y%m%d") if self.date is None else self.date
         movieName = reSub('[^0-9a-zA-Z ]+', '', movieData.get('TITLE') ).lower().replace( " ", "-" )
 
         movieUrl = "https://in.bookmyshow.com/buytickets/"
@@ -57,7 +59,30 @@ class BookMyShow( object ):
         print( movieUrl )
         return movieUrl
 
-    def search( self, searchTerm, typeName="Movies" ):
+    def getCinemaUrl( self, cinemaData ):
+        '''
+        `cinemaData` is similar to below:
+
+        {"CC":"PVR","ST":"NS","REGION_SLUG":"","GRP":"Venue","EVENTSTRTAGS":[],"RATING":"","L_URL":"","WTS":"","TITLE":"PVR: Forum Mall, Koramangala","ID":"PVBN","TYPE_NAME":"Venues","TYPE":"|MT|","CAT":""}
+        '''
+
+        curDate = datetime.now().strftime("%Y%m%d") if self.date is None else self.date
+        cinemaName = reSub('[^0-9a-zA-Z ]+', '', cinemaData.get('TITLE') ).lower().replace( " ", "-" )
+
+        cinemaUrl = "https://in.bookmyshow.com/buytickets/"
+        cinemaUrl += cinemaName
+        cinemaUrl += "/cinema-" + self.regionData.get( 'code' ).lower() + "-"
+        cinemaUrl += cinemaData.get( 'ID' )
+        cinemaUrl += "-MT/"
+        cinemaUrl += curDate
+        print( cinemaUrl )
+        return cinemaUrl
+
+    def getUrlDataJSON( self, searchTerm ):
+        '''
+        returns all matched results after searching on bms
+        '''
+
         url = self.getSearchUrl( searchTerm )
         headers = {
                 'x-requested-from' : 'WEB',
@@ -65,21 +90,28 @@ class BookMyShow( object ):
         data = self.ss.get( url, headers=headers )
         assert data.status_code == 200
         jsonResp = data.json()
+        return jsonResp
 
+    def search( self, searchTerm, typeName="Movies" ):
+        jsonResp = self.getUrlDataJSON( searchTerm )
         # return the first hit belonging to typeName
-        movieData = {}
+        data = {}
         for movieInfo in jsonResp[ 'hits' ]:
             if movieInfo.get( 'TYPE_NAME' ) == typeName:
-                movieData = movieInfo
+                data = movieInfo
                 break
         
-        # Check if show is on
-        if typeName == "Movies" and movieData.get( 'SHOWDATE' ) is None:
-            print( "The show counters are yet to be opened!" )
-            return None
+        url = None
+        if typeName == "Movies":
+            if data.get( 'SHOWDATE' ) is None:
+                # Check if show is on
+                print( "The show counters are yet to be opened!" )
+                return None
+            url = self.getMovieUrl( data )
+        elif typeName == "Venues":
+            url = self.getCinemaUrl( data )
 
-        movieUrl = self.getUrl( movieData )
-        return movieUrl
+        return url
     
     def checkAvailability( self, movieLink ):
         '''
@@ -87,10 +119,34 @@ class BookMyShow( object ):
         '''
         pass
 
-    def checkCinemaAvailability( self, movieName ):
+    def checkCinemaAvailability( self, cinemaLink, movieName ):
         '''
         Rings a bell if a show is available in your requested cinema
         '''
+        cinemaDetails = self.ss.get( cinemaLink )
+        assert cinemaDetails.status_code == 200
+        cinemaSoup = BeautifulSoup( cinemaDetails.content, 'html5lib' )
+        # get movie name
+        jsonRespOfMovies = self.getUrlDataJSON( movieName )
+        # return the first hit belonging to movieName
+        data = {}
+        for movieInfo in jsonRespOfMovies[ 'hits' ]:
+            if movieInfo.get( 'TYPE_NAME' ) == "Movies":
+                data = movieInfo
+                break
+        movieName = data.get( 'TITLE', movieName )
+
+        # find if your requested cinema is in the list
+        found = False
+        for movieTitle in cinemaSoup.find_all( "strong" ):
+            if movieTitle.getText().strip().lower() == movieName.lower():
+                found = True
+                break
+        if found:
+            # Movie tickets are now available
+            print( "HURRAY! Movie tickets are now available" )
+        else:
+            print( "Movie tickets aren't available yet" )
         
     def checkMovie( self, name ):
         movieLink = self.search( name )
@@ -102,9 +158,10 @@ class BookMyShow( object ):
         cinemaLink = self.search( name, typeName="Venues" )
         if cinemaLink is None:
             exit( 0 )
-        self.checkCinemaAvailability( movieName )
+        self.checkCinemaAvailability( cinemaLink, movieName )
 
 if __name__ == "__main__":
-    bms = BookMyShow( regionCode="BANG" )
+    bms = BookMyShow( regionCode="BANG", date="20190419" )
     # bms.check( name="Avengers: Endgame" )
-    bms.checkMovie( name="hellboy" )
+    # bms.checkMovie( name="hellboy" )
+    bms.checkCinema( name="PVR Forum Mall Koramangala", movieName="The Curse Of The Weeping Woman" )
