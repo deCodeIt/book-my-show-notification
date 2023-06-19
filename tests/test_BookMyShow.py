2f7e4b03a0c6b8e63b6a84b979c376c6a33676c6
deCodeIt <shaikhali.jafri@iitrpr.ac.in>
2023-06-19 20:08:36 +0530

    Add test for region and venue

--- tests/test_BookMyShow.py
+++ tests/test_BookMyShow.py
@@ -1,12 +1,34 @@
 from BookMyShow import BMS
 
+class MockParsedArgs:
+  def __init__( self, **kwargs ):
+    for key, value in kwargs.items():
+      setattr( self, key, value )
+
 def test_region():
-  args = {
-    'movie': 'Random Movie',
-    'cinema': 'Random Cinema',
-    'regionCode': 'bang',
-  }
+  args = MockParsedArgs(
+    movie='Random Movie',
+    cinema= 'Random Cinema',
+    regionCode= 'bang',
+    date=None,
+    format=None,
+    alarm=None
+  )
   bms = BMS( args )
   regions = bms.searchRegion( args.regionCode )
-  assert len( regions ) >= 1
-
+  assert any( [ True if region.RegionCode.lower() == args.regionCode else False for region in regions ] )
+  
+def test_venue():
+  args = MockParsedArgs(
+    movie='Random Movie',
+    cinema= 'pvbn',
+    regionCode= 'BANG',
+    date=None,
+    format=None,
+    alarm=None
+  )
+  bms = BMS( args )
+  regions = bms.searchRegion( args.regionCode )
+  bms.setRegion( regions[ 0 ] )
+  venues = bms.searchVenue( args.cinema )
+  assert any( [ True if venue.VenueCode.lower() == args.cinema else False for venue in venues ] )
