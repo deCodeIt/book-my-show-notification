0f40201e5f755a391f814a6f3d77d68c73a293a7
deCodeIt <shaikhali.jafri@iitrpr.ac.in>
2023-06-19 20:14:52 +0530

    Combine redundant tests

--- tests/test_BookMyShow.py
+++ tests/test_BookMyShow.py
@@ -5,10 +5,38 @@ class MockParsedArgs:
     for key, value in kwargs.items():
       setattr( self, key, value )
 
-def test_region():
+# def test_region():
+#   args = MockParsedArgs(
+#     movie='Random Movie',
+#     cinema= 'Random Cinema',
+#     regionCode= 'bang',
+#     date=None,
+#     format=None,
+#     alarm=None
+#   )
+#   bms = BMS( args )
+#   regions = bms.searchRegion( args.regionCode )
+#   assert any( [ True if region.RegionCode.lower() == args.regionCode else False for region in regions ] )
+  
+# def test_venue():
+#   args = MockParsedArgs(
+#     movie='Random Movie',
+#     cinema= 'pvbn',
+#     regionCode= 'BANG',
+#     date=None,
+#     format=None,
+#     alarm=None
+#   )
+#   bms = BMS( args )
+#   regions = bms.searchRegion( args.regionCode )
+#   bms.setRegion( regions[ 0 ] )
+#   venues = bms.searchVenue( args.cinema )
+#   assert any( [ True if venue.VenueCode.lower() == args.cinema else False for venue in venues ] )
+  
+def test_cinema_url():
   args = MockParsedArgs(
     movie='Random Movie',
-    cinema= 'Random Cinema',
+    cinema= 'pvbn',
     regionCode= 'bang',
     date=None,
     format=None,
@@ -17,35 +45,10 @@ def test_region():
   bms = BMS( args )
   regions = bms.searchRegion( args.regionCode )
   assert any( [ True if region.RegionCode.lower() == args.regionCode else False for region in regions ] )
-  
-def test_venue():
-  args = MockParsedArgs(
-    movie='Random Movie',
-    cinema= 'pvbn',
-    regionCode= 'BANG',
-    date=None,
-    format=None,
-    alarm=None
-  )
-  bms = BMS( args )
-  regions = bms.searchRegion( args.regionCode )
   bms.setRegion( regions[ 0 ] )
   venues = bms.searchVenue( args.cinema )
   assert any( [ True if venue.VenueCode.lower() == args.cinema else False for venue in venues ] )
-  
-def test_cinema_url():
-  args = MockParsedArgs(
-    movie='Random Movie',
-    cinema= 'pvbn',
-    regionCode= 'BANG',
-    date=None,
-    format=None,
-    alarm=None
-  )
-  bms = BMS( args )
-  regions = bms.searchRegion( args.regionCode )
-  bms.setRegion( regions[ 0 ] )
-  venues = bms.searchVenue( args.cinema )
   bms.setVenue( venues[ 0 ] )
   url = bms.getCinemaUrl()
   assert url.startswith( 'https://in.bookmyshow.com/buytickets/pvr-nexus-formerly-forum-koramangala/cinema-bang-PVBN-MT/' )
+  assert bms.fetchCinemaPage() is not None
